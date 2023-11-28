4781e316be028eba78883793c717401291500625
spantheslayer <supriyosarkar282@gmail.com>
2023-11-28 18:25:14 +0530

    Init 🚀
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from 'next-auth'
 import { SessionProvider } from 'next-auth/react'
 import { type AppType } from 'next/app'
 
+import font from '~/lib/fonts/font'
 import '~/styles/globals.css'
 import { api } from '~/utils/api'
 
@@ -10,9 +11,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
   pageProps: { session, ...pageProps }
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        body {
+          font-family: ${font.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   )
 }
 
